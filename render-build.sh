ffe52a0092d73253743061bdd90081b6894b2221
Carelinehq <carelinehq@gmail.com>
2025-07-21 00:49:25 +0000

    Make render-build.sh executable

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-set -e
 
+# Make sure the script has execute permissions:
+# chmod +x render-build.sh
+
+echo "Installing global tools..."
 npm install -g rimraf @types/jest vite @microsoft/api-extractor @microsoft/api-documenter ts-node
+
+echo "Running npm ci..."
 npm ci
+
+echo "Installing additional dependencies..."
 npm install --no-save @types/jest vite
+
+echo "Installing core workspace deps..."
 npm --workspace packages/core install --no-save @types/jest vite
+
+echo "Running turbo build..."
 npx turbo run build --filter=@medplum/server
